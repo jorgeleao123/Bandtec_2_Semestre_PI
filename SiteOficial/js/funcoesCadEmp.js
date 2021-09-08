640ff785ccac61401a4ef48c84baa598cd93b02a
flamauricio <51135272+flamauricio@users.noreply.github.com>
2021-09-07 22:20:27 -0300

    subindo toda validação em js

---
+++ SiteOficial/js/funcoesCadEmp.js
@@ -0,0 +1,16 @@
+// Funções 
+function cadastrarEmp(){
+    var nomeEmpresa = nomeEmp.value;
+    var cnpj = cnpjEmp.value;
+    var senhaEmpresa = senhaEmp.value;
+            if (nomeEmpresa == "" || senhaEmpresa == "") {
+                alert("Favor informar Razão Social!");
+                setTimeout (300);
+            }
+            else if(cnpj == ""){
+                alert("Favor informar corretamente seu CNPJ");
+            }
+            else{
+                window.location.href ="login_empresa.html";
+            }
+}
