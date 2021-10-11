39518cbf0afda00f026f9d41e098ef34cc7d4093
brunopmendes <brupmendes@icloud.com>
2021-10-11 19:32:15 -0300

    Fix(usuario.ts): alterando parâmetros de requisição da API, conectando API de login e cadastro com BD

--- SiteOficial/config/config.js
+++ SiteOficial/config/config.js
@@ -26,7 +26,7 @@ module.exports = {
   dev: {
     // altere APENAS username, password e database.
     username: 'root',
-    password: 'jotalion02',
+    password: '',
     database: 'sprintTotemHub',
     host: '127.0.0.1',
     dialect: 'mysql',
